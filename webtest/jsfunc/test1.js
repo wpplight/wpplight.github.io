d72fa76b845ea23067cd25ee92b03de7a58b6424
wpplight <2023040093@buct.edu.cn>
2024-10-26 23:03:08 +0800

    Update test1.js

--- webtest/jsfunc/test1.js
+++ webtest/jsfunc/test1.js
@@ -1,10 +1,14 @@
 function xishua() {
     document.getElementById('momo').src = 'imagefile/dragon.png';
-    document.getElementById('demo').innerHTML = '联系个蛋啊，真以为我能负责啊？🤣';
+    document.getElementById('demo').innerHTML = '联系个蛋啊，真以为我能负责啊？等我们做大做强的再负责。🥹';
     document.getElementById('but1').style.display = 'none';
     document.getElementById('but2').style.display = 'block';
+   
+    
 }
+
 function zhencheng() {
-    document.getElementById('demo').innerHTML = "等我们做大做强的再负责。🥹";
+    
+    alert('不许再点了！🤞😊')
     document.getElementById('but2').style.display = 'none';
 }
