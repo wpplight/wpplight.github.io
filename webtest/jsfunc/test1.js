d5fb42a00f3d9efa94716a1e05e8710ca45ab19a
wpplight <2023040093@buct.edu.cn>
2024-10-26 20:58:54 +0800

    more content
    new ui
    new contast

---
+++ webtest/jsfunc/test1.js
@@ -0,0 +1,5 @@
+function xishua() {
+    document.getElementById('momo').src = 'imagefile/dragon.png';
+    document.getElementById('demo').innerHTML = '联系个蛋啊，真以为我能负责啊？等我们做大做强的再负责。🥹';
+    document.getElementById('but1').style.display = 'none';
+}
