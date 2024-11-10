0ea4cded571b05dc00752902bbab9b7f6117d792
wpplight <2023040093@buct.edu.cn>
2024-11-11 06:03:17 +0800

    very new mune

--- webtest/jsfunc/test1.js
+++ webtest/jsfunc/test1.js
@@ -3,8 +3,6 @@ function xishua() {
     document.getElementById('demo').innerHTML = '联系个蛋啊，真以为我能负责啊？';
     document.getElementById('but1').style.display = 'none';
     document.getElementById('but2').style.display = 'block';
-   
-    
 }
 
 function check_a()
@@ -31,5 +29,16 @@ function check_b()
     }
 }
 
+function handleloadClick() { 
+window.location.href = 'load.html';
+}
+function handleloadClick1() {
+    window.location.href = 'webtest/load.html';
+}
+
+const but1 = document.getElementById('dengl1');
+if (but1)  but1.addEventListener('click', handleloadClick1);
+const but = document.getElementById('dengl');
+if(but) but.addEventListener('click', handleloadClick);
 
 
